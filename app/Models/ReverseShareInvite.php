445c62b5a6c84e4242dded1d1be589912f6ccb96
kongiss46 <kongiss@gmail.com>
2026-04-29 20:26:04 +0200

    Update ReverseShareInvite.php

--- app/Models/ReverseShareInvite.php
+++ app/Models/ReverseShareInvite.php
@@ -9,6 +9,7 @@ class ReverseShareInvite extends Model
   protected $fillable = [
     'user_id',
     'guest_user_id',
+    'guest_token',
     'recipient_name',
     'recipient_email',
     'message',
